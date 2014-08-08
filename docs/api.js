73364dc56e0288cbb3d1fb79ab1985af5672c68e
root <root@dun-jenkins02.(none)>
2014-08-08 10:52:20 +0000

    Creating latest release of Console from Jenkins build 50

---
+++ docs/api.js
@@ -0,0 +1,13 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "GettingStarted",
+        "PhaserNotes",
+        "iConsole.ads",
+        "iConsole.game",
+        "iConsole.periodical"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
