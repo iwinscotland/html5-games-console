4553265e588f1c8705c53f79cc4e62a16ad2bd2d
root <root@dun-jenkins02.(none)>
2014-10-03 14:56:25 +0000

    Creating latest release of Console from Jenkins build 91

--- docs/api.js
+++ docs/api.js
@@ -2,7 +2,6 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "GettingStarted",
-        "PhaserNotes",
         "iConsole.ads",
         "iConsole.game",
         "iConsole.periodical"
